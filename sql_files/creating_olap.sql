3dbcad00fa2581a48067f89ea3ad082aa6c7f95c
Samyak Jain <142383509+SamyakJain-DS@users.noreply.github.com>
2025-07-27 22:38:42 +0530

    Update creating_olap.sql

--- sql_files/creating_olap.sql
+++ sql_files/creating_olap.sql
@@ -1,21 +1,20 @@
-CREATE DATABASE project_olap;
-USE project_olap;
+CREATE DATABASE OLAP;
+USE OLAP;
 
--- CREATE TABLE tablets_uncleaned LIKE project.tablets_uncleaned;
--- CREATE TABLE laptops_uncleaned LIKE project.laptops_uncleaned;
--- CREATE TABLE mobiles_uncleaned LIKE project.mobiles_uncleaned;
+CREATE TABLE tablets_uncleaned LIKE defaultdb.tablets_uncleaned;
+CREATE TABLE laptops_uncleaned LIKE defaultdb.laptops_uncleaned;
+CREATE TABLE mobiles_uncleaned LIKE defaultdb.mobiles_uncleaned;
 
--- CREATE TABLE tablets LIKE project.tablets;
--- CREATE TABLE laptops LIKE project.laptops;
--- CREATE TABLE mobiles LIKE project.mobiles;
+CREATE TABLE tablets LIKE defaultdb.tablets;
+CREATE TABLE laptops LIKE defaultdb.laptops;
+CREATE TABLE mobiles LIKE defaultdb.mobiles;
 
--- INSERT INTO tablets_uncleaned (SELECT * FROM project.tablets_uncleaned);
+INSERT INTO tablets_uncleaned (SELECT * FROM defaultdb.tablets_uncleaned);
 
--- INSERT INTO laptops_uncleaned (SELECT * FROM project.laptops_uncleaned);
+INSERT INTO laptops_uncleaned (SELECT * FROM defaultdb.laptops_uncleaned);
 
--- INSERT INTO mobiles_uncleaned (SELECT * FROM project.mobiles_uncleaned);
+INSERT INTO mobiles_uncleaned (SELECT * FROM defaultdb.mobiles_uncleaned);
 
--- INSERT INTO tablets (SELECT * FROM project.tablets);
--- INSERT INTO laptops (SELECT * FROM project.laptops);
--- INSERT INTO mobiles (SELECT * FROM project.mobiles);
-
+INSERT INTO tablets (SELECT * FROM defaultdb.tablets);
+INSERT INTO laptops (SELECT * FROM defaultdb.laptops);
+INSERT INTO mobiles (SELECT * FROM defaultdb.mobiles);
